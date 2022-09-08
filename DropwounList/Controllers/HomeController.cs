bbde1c26a99d99c34060fa5be42ddf114fa25ee6
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-09-09 02:03:09 +0530

    Add project files.

---
+++ DropwounList/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using DropwounList.Db;
+using DropwounList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace DropwounList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            mydbEntities obj = new mydbEntities();
+            var res = obj.Drops.ToList();
+            List<Class1> c = new List<Class1>();
+            foreach (var item in res)
+            {
+                c.Add(new Class1
+                {
+                    id=item.id,
+                    name=item.name,
+                    roll=item.roll,
+                    city=item.city
+                });
+            }
+            return View(c);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult form()
+        {
+
+            List<string> list = new List<string>()
+            {
+                "noida","delhi","Uk"
+            };
+            ViewBag.l = list;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult form(Class1 cobj)
+        {
+            mydbEntities obj = new mydbEntities();
+            Drop tbl = new Drop();
+            tbl.id = cobj.id;
+            tbl.name = cobj.name;
+            tbl.roll = cobj.roll;
+            tbl.city = cobj.city;
+
+            obj.Drops.Add(tbl);
+            obj.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ DropwounList/Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace DropwounList.Models
+{
+    public class Class1
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public Nullable<int> roll { get; set; }
+        [Required]
+        public string city { get; set; }
+    }
+}
